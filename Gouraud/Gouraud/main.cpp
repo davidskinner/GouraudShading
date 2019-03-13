46cf14a61fc4e149fb2a67cf8821132726406ed0
Unknown <dskinner@uark.edu>
2019-03-12 20:37:28 -0500

    getting there

--- Gouraud/Gouraud/main.cpp
+++ Gouraud/Gouraud/main.cpp
@@ -19,12 +19,8 @@ public:
     float y;
     float z;
     
-    V3(float x, float y, float z)
-    {
-        this->x = x;
-        this->y = y;
-        this->z =z;
-    }
+    V3(float x, float y, float z) : x(x),y(y)
+    {}
 };
 
 class RGB
@@ -34,22 +30,21 @@ public:
     float green;
     float blue;
     
-    RGB(float r, float g, float b)
-    {
-        this->red = r;
-        this->green = g;
-        this->blue = b;
-    }
+    RGB(float r, float g, float b) : red(r), green(g), blue(b)
+    {}
 };
 
-V3 L1 = V3(3,5,1);
+
+
+V3 L1 = V3(0,0,1);
 V3 L2 = V3(0,0,0);
+
+float a = .1;
+float b = .09;
+float c = .2;
 
 RGB LightOneColor = RGB(0.1,0.2,0.3);
 RGB LightTwoColor = RGB(0.9,0.8,0.7);
-
-
-
 
 // Transformation variables
 #define ROTATE 1
@@ -72,10 +67,9 @@ float Ny[SIZE + 1][SIZE + 1];
 float Nz[SIZE + 1][SIZE + 1];
 #define STEP 0.1
 
-
-
-// 1/(a+bD+cD2)
-
+float red[SIZE +1][SIZE +1];
+float green[SIZE +1][SIZE +1];
+float blue[SIZE +1][SIZE +1];
 
 //---------------------------------------
 // Initialize surface points
@@ -136,18 +130,7 @@ void init_normals() // big boi
         }
 }
 
-//doLighting
-void doLighting()
-{
-    //calculate vectors from all points to light L1
-    for (int i=0; i<=SIZE; i++)
-    {
-        for (int j=0; j<=SIZE; j++)
-        {
-            //find vector from vertex to light source
-        }
-    }
-}
+
 
 //---------------------------------------
 // Init function for OpenGL
@@ -166,9 +149,78 @@ void init()
     init_normals();
 }
 
-//---------------------------------------
-// Display callback for OpenGL
-//---------------------------------------
+//red(i,j)
+//green(i,j)
+//blue(i,j)
+
+
+
+V3 calculateV(V3 light, float x, float y, float z)
+{
+    // x2-x1, y2-y1, z2-z1
+    V3 temp = V3(light.x - x, light.y - y, light.z - z);
+    return temp;
+}
+
+void dotProduct()
+{
+    //x1*x2 + y1*y2 + z1*z2
+    
+    
+}
+
+
+
+void doLighting()
+{
+    V3 v = V3(1,2,3);
+    float magnitudeOne = 3;
+    float magnitudeTwo = 5;
+    float dotProduct;
+    float EuclideanD;
+    float inverseSquare;
+    
+    for (int i =0; i <= SIZE; i++) {
+        for (int j = 0; j <= SIZE; j++) {
+            //v
+            v = V3(L1.x - Px[i][j], L1.y - Py[i][j], L1.z - Pz[i][j]);
+            
+            //v*n
+            dotProduct = v.x * Nx[i][j] + v.y * Ny[i][j] + v.z * Nz[i][j];
+            
+            EuclideanD = sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
+            
+            // 1/(a+bD+cD2)
+            inverseSquare = 1/(a + b * EuclideanD + c * EuclideanD * EuclideanD);
+            
+            magnitudeOne = dotProduct * inverseSquare;
+        
+            //////////////////////////////
+            //v
+            v = V3(L2.x - Px[i][j], L2.y - Py[i][j], L2.z - Pz[i][j]);
+            
+            
+            //v*n
+            
+            dotProduct = v.x * Nx[i][j] + v.y * Ny[i][j] + v.z * Nz[i][j];
+            
+            EuclideanD = sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
+            
+            // 1/(a+bD+cD2)
+            inverseSquare = 1/(a + b * EuclideanD + c * EuclideanD * EuclideanD);
+
+            magnitudeTwo = dotProduct * inverseSquare;
+            
+            red[i][j] = (magnitudeOne * LightOneColor.red) + (magnitudeTwo * LightTwoColor.red);
+            green[i][j] = (magnitudeOne * LightOneColor.green) + (magnitudeTwo * LightTwoColor.green);
+            blue[i][j] = (magnitudeOne * LightOneColor.blue) + (magnitudeTwo * LightTwoColor.blue);
+        }
+    }
+
+}
+
+//three 2d arrays
+
 void display()
 {
     // Incrementally rotate objects
@@ -179,21 +231,22 @@ void display()
     glRotatef(yangle, 0.0, 1.0, 0.0);
     glRotatef(zangle, 0.0, 0.0, 1.0);
     
+    doLighting();
     
     // Draw the surface
     int i, j;
     for (i = 0; i < SIZE; i++)
         for (j = 0; j < SIZE; j++)
         {
-            // glBegin(GL_LINE_LOOP);
+            
             glBegin(GL_POLYGON);
-            glNormal3f(Nx[i][j], Ny[i][j], Nz[i][j]); // glColor(red(),green(),blue())
+            glColor3f(red[i][j], green[i][j], blue[i][j]); // glColor(red(),green(),blue())
             glVertex3f(Px[i][j], Py[i][j], Pz[i][j]);
-            glNormal3f(Nx[i + 1][j], Ny[i + 1][j], Nz[i + 1][j]);
+            glColor3f(red[i + 1][j], green[i + 1][j], blue[i + 1][j]);
             glVertex3f(Px[i + 1][j], Py[i + 1][j], Pz[i + 1][j]);
-            glNormal3f(Nx[i + 1][j + 1], Ny[i + 1][j + 1], Nz[i + 1][j + 1]);
+            glColor3f(red[i + 1][j + 1], green[i + 1][j + 1], blue[i + 1][j + 1]);
             glVertex3f(Px[i + 1][j + 1], Py[i + 1][j + 1], Pz[i + 1][j + 1]);
-            glNormal3f(Nx[i][j + 1], Ny[i][j + 1], Nz[i][j + 1]);
+            glColor3f(red[i][j + 1], green[i][j + 1], blue[i][j + 1]);
             glVertex3f(Px[i][j + 1], Py[i][j + 1], Pz[i][j + 1]);
             glEnd();
         }
